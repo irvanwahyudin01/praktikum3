f2728e976ead0e34d9dbb1710d0c1e0b5afff40b
irvanwahyudin01 <59524240+irvanwahyudin01@users.noreply.github.com>
2025-10-21 21:00:17 +0700

    Add files via upload

---
+++ praktikum/latihan2.py
@@ -0,0 +1,6 @@
+a=int(input("masukan nilai a :"))
+b=int(input("masukan nilai b :"))
+print("variable a=",a)
+print("variable b=",b)
+print("hasil penggabungan {1}&{0}=%d".format(a,b) %(a+b))
+print("hasil pembagian {1}/{0}=%d".format(a,b) %(a/b))
